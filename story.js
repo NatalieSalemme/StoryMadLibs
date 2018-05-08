1c1b91935837f5f3423acb2b1653317cca391278
NatalieSalemme <36144430+NatalieSalemme@users.noreply.github.com>
2018-05-08 16:56:55 -0700

    Create story.js

---
+++ story.js
@@ -0,0 +1,8 @@
+var adjective = prompt("Please type an adjective")
+var verb = prompt("Please type a verb");
+var noun = prompt("Please type a noun");
+alert("All done!");
+document.write("Once upon a time there was a very " + adjective + " princess. 
+She went all around her village because she wanted to " + verb + ". " + 
+"She spent all day trying to find something to do, 
+so once she was bored she went back to her castle to build her own village made out of " + noun + ".");
