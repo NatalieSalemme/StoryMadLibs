4a098a589f2918bf673121a4efeede24c32eec20
Natalie Salemme <nataliesalemme@gmail.com>
2018-09-30 09:37:17 -0700

    Adding story

--- story.js
+++ story.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -13,7 +12,8 @@ class App extends Component {
       bodypart: '',
       drink: '',
       number: '',
-      season: ''
+      season: '',
+      completedForm: false
     }
     this.place = this.place.bind(this);
     this.color = this.color.bind(this);
@@ -67,42 +67,62 @@ class App extends Component {
   }
   submit(e) {
     e.preventDefault();
-    if(this.number) {
-      return <p>hello</p>
+    let { place, color, verb, pet, bodypart, drink, number, season } = this.state;
+    if(!place || !color || !verb || !pet || !bodypart || !drink || !number || !season) {
+      alert('Please fill in all boxes');
+    } else {
+      this.setState({
+        completedForm: true
+      });
     }
-
   }
   render() {
-    return (
-      <div className="App">
-        <h1>Mad Libs</h1>
-        <p>Please fill in the boxes and create your own story.</p>
-        <div class="row">
-        <input type="text" placeholder="Name of a place" onChange={this.place}/>
-        <input type="text" placeholder="Color" onChange={this.color} />
-        <input type="text" placeholder="Verb" onChange={this.verb} />
-        <input type="text" placeholder="Name for a pet" onChange={this.pet} />
-      </div>
-      <div class="row">
-        <input type="text" placeholder="Body part" onChange={this.bodypart} />
-        <input type="text" placeholder="Drink" onChange={this.drink} />
-        <input type="number" placeholder="Number" onChange={this.number} />
-        <input type="text" placeholder="Season of the year" onChange={this.season} />
-      </div>
-      <input type="submit" value="Create Story" onSubmit={this.submit}/>
-      <div>
-        <h3>Your story</h3>
-          <p class="container"> Once upon a time, there was a turtle who I named
-            {this.state.pet}. It decided it wanted to {this.state.verb} so I really
-            had to try hard to catch up to it. I was so thirsty so I decided to drink
-            {this.state.drink} to quench my thirst. It was <br /> {this.state.season} and
+    if(this.state.completedForm) {
+      return (
+      <div style={{backgroundColor: 'lightblue'}}>
+        <h3 style={{textAlign: 'center', color: 'Purple'}}>Your Personalized Story</h3>
+        <img src="src/madlibs.jpg" />
+          <p className="paragraphContainer"> Once upon a time, there was a turtle who I named {this.state.pet}.
+            It decided it wanted to {this.state.verb} so I really
+            had to try hard to catch up to it. I was so thirsty after that so I decided to
+            drink {this.state.drink} to quench my thirst. It was {this.state.season} and
             I was definitely NOT dressed properly for the weather that day. Once I
-            relaxed a little bit, I looked at the turtle and saw that there was a
-            {this.state.color} envelope attached to its {this.state.bodypart}. I
+            relaxed a little bit, I looked at the turtle and saw that there was
+            a {this.state.color} envelope attached to its {this.state.bodypart}. I
             couldn't believe it, attached were {this.state.number} tickets to my
             favorite place, {this.state.place}. I was so excited I immediately
             tweeted about it.</p>
       </div>
+    );
+    }
+    return (
+      <div className="App">
+        <h1>Mad Libs</h1>
+        <p>Please fill in the boxes and create your own story.</p>
+        <form onSubmit={this.submit} className="form">
+        <div className="row">
+          <div className="column">
+        <input type="text" placeholder="Name of a place" onChange={this.place}/>
+        <input type="text" placeholder="Color" onChange={this.color} />
+      </div>
+      <div className="column">
+        <input type="text" placeholder="Verb" onChange={this.verb} />
+        <input type="text" placeholder="Name for a pet" onChange={this.pet} />
+      </div>
+      </div>
+      <div className="row">
+        <div className="column">
+        <input type="text" placeholder="Body part" onChange={this.bodypart} />
+        <input type="text" placeholder="Drink" onChange={this.drink} />
+      </div>
+      <div className="column">
+        <input type="number" placeholder="Number" onChange={this.number} />
+        <input type="text" placeholder="Season of the year" onChange={this.season} />
+      </div>
+      </div>
+      <input type="submit" value="Create Story" onClick={this.submit}/>
+    </form>
+
       </div>
     );
   }
